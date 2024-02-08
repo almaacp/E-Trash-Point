4f3ba7a4aab4b52b3d63ea30993bf476160c5fcb
almaacp <almaalyaciptaputri@gmail.com>
2024-02-08 23:42:25 +0700

    ubah logo

--- resources/views/front.blade.php
+++ resources/views/front.blade.php
@@ -8,14 +8,24 @@
     <link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.min.css') }}">
 </head>
 <body>
-    <div class="container">
+    <div class="container-fluid">
         <div class="mt-3">
             <nav class="navbar navbar-expand-lg bg-body-tertiary">
                 <div class="container-fluid">
-                    <a href=""><img style="width: 300px" src="{{ asset('gambar/logo-judul-removebg.png') }}" alt=""></a>
+                    <a href=""><img style="width: 350px" src="{{ asset('gambar/logo-judul-removebg.png') }}" alt=""></a>
                     <ul class="navbar-nav gap-3">
-                        <li class="nav-item"><span class="badge text-bg-secondary" style="font-size: medium">Register</span></li>
-                        <li class="nav-item"><span class="badge text-bg-primary" style="font-size: medium">Login</span></li>
+
+                        <li class="nav-item">
+                            <a href="">
+                                <span class="badge text-bg-secondary" style="font-size: medium">Register</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="">
+                                <span class="badge text-bg-primary" style="font-size: medium">Login</span>
+                            </a>
+                        </li>
+                        
                     </ul>
                 </div>
             </nav>
@@ -27,7 +37,6 @@
             A&B Project
         </div>
     </div>
-    
     <script src="{{ asset('bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 </body>
 </html>
