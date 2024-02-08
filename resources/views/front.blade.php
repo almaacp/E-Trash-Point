c66f995c256c8f1c7251e7c8c1cf779ec6ddcfec
almaacp <almaalyaciptaputri@gmail.com>
2024-02-08 10:02:10 +0700

    tambah front blade

---
+++ resources/views/front.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>E-Trash Point</title>
+    <link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.min.css') }}">
+</head>
+<body>
+    <div class="container">
+        <div class="mt-3">
+            <nav class="navbar navbar-expand-lg bg-body-tertiary">
+                <div class="container-fluid">
+                    <a href=""><img style="width: 300px" src="{{ asset('gambar/logo-judul-removebg.png') }}" alt=""></a>
+                    <ul class="navbar-nav gap-5">
+                        <li class="nav-item">Register</li>
+                        <li class="nav-item">Login</li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+        <div>
+            ini isinya
+        </div>
+        <div>
+            A&B Project
+        </div>
+    </div>
+    
+    <script src="{{ asset('bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+</body>
+</html>
